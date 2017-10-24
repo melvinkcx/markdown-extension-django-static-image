7816bf67959cf078d3742078ba3e07adae8b7463
melvin <melvinkcx@gmail.com>
2017-10-24 14:35:53 +0800

    Added setup.py, ready to release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='markdown-djangostaticimage',
+      version='0.1',
+      description='Markdown extension: Add static tag to image source',
+      url='https://bitbucket.org/melvinkcx/markdown-extension-django-static-image',
+      author='Melvin Koh',
+      author_email='melvinkcx@hotmail.com',
+      license='MIT',
+      packages=['django_static_image'],
+      keywords="django markdown static",
+      zip_safe=False)
